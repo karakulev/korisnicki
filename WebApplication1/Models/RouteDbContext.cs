61d258a2126640e1a2b0683274c82a3efc2eb452
Micevski <davidkocani123@gmail.com>
2019-08-13 20:58:43 +0200

    Inital commit

---
+++ WebApplication1/Models/RouteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class RouteDbContext: DbContext
+    {
+        public DbSet<Route> routes { get; set; }
+
+        public RouteDbContext() : base("Route") { }
+    }
+}
